dab1d5d5f23f85937d5a7159f7bcba914db160dc
codevictory <main@codevictory.net>
2020-08-25 16:01:59 +0300

    Initial project skeleton.

---
+++ ModelContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ef_core_team_migrations_demo_model
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Pharaoh> Pharaohs { get; set; }
+    }
+}
---
+++ Pharaoh.cs
@@ -0,0 +1,8 @@
+namespace ef_core_team_migrations_demo_model
+{
+    public class Pharaoh
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
